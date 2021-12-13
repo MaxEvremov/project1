3a19eb88e67b33137cd85c16701a6b59bde694ef
Maxim <maximevremov@gmail.com>
2021-12-13 19:14:13 +0500

    работает все, кроме сортировки и поиска.

--- src/hooks/useSortedAndSearchedTasks.js
+++ src/hooks/useSortedAndSearchedTasks.js
@@ -1,20 +1,11 @@
+
 import { ref, computed } from 'vue'
 
 export default function useSortedTasks (sortedTasks) {
   const searchQuery = ref('')
-  let sortedAndSearchedTasks = ref([])
-  const sorted = (query) => {
-    if (sortedTasks) {
-      sortedAndSearchedTasks = computed(() => {
-        return sortedTasks.value.filter(task => task.value.toLowerCase().includes(query.toLowerCase()))
-      })
-      console.log('Поле сортировки не пустое')
-    } else {
-      sortedAndSearchedTasks = sortedTasks
-      console.log('поле сортировки пустое')
-    }
-  }
-  sorted(searchQuery.value)
+  const sortedAndSearchedTasks = computed(() => {
+    return sortedTasks.value.filter(task => task.title.toLowerCase().includes(searchQuery.value.toLowerCase()))
+  })
   return {
     searchQuery, sortedAndSearchedTasks
   }
--- src/hooks/useSortedTasks.js
+++ src/hooks/useSortedTasks.js
@@ -1,22 +1,10 @@
 import { ref, computed } from 'vue'
 
 export default function useSortedTasks (tasks) {
-  console.log(tasks.value)
-  console.log(tasks)
   const selectedSort = ref('')
-  let sortedTasks = ref([])
-  const sorting = (value) => {
-    if (value) {
-      sortedTasks = computed(() => {
-        return [tasks.value].sort((task1, task2) => task1[selectedSort.value]?.localCompare(task2[selectedSort.value]))
-      })
-      console.log('Поле сортировки не пустое')
-    } else {
-      sortedTasks = tasks
-      console.log('поле сортировки пустое')
-    }
-  }
-  sorting(selectedSort.value)
+  const sortedTasks = computed(() => {
+    return [...tasks.value].sort((task1, task2) => task1[selectedSort.value]?.localCompare(task2[selectedSort.value]))
+  })
   return {
     selectedSort, sortedTasks
   }
